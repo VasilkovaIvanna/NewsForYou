5f864ffefdac7cef45feca6e3ba9c92d77f138d3
Ivanna Vasilkova <ivanna.vasilkova@wolkov.consulting>
2021-12-05 14:25:25 +0200

    Initial Commit

---
+++ NewsForYou/NewsForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsForYouApp.swift
+//  NewsForYou
+//
+//  Created by Ivy on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
